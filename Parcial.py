b333c2a308a5e62dcef5bb2d02cc4109138732fe
Jhon Sebastian Garcia <jhons.garciab@uqvirtual.edu.co>
2023-03-22 17:18:44 -0500

    Intercambio de líneas

--- Parcial.py
+++ Parcial.py
@@ -61,31 +61,35 @@ class Matriz:
 
 #Ahora defino mul y rmul para poder hacer las operaciones entre matrices al momento de G-J
   def __mul__(self, otra):
-    if self.shape[0] != self.shape[1]:
-      print("Las matrices no pueden multiplicarse")
-      
-    resultado = [[0]*otra.shape[0] for _ in range(self.shape[1])] #Matriz llena de 0 con las mismas dimensiones para llevar a cabo las mult.
+    if self.shape[0] != otra.shape[1]:
+        print("Las matrices no pueden multiplicarse")
+        return None
+    resultado = [[0]*otra.shape[1] for _ in range(self.shape[0])] #Matriz llena de 0 con las mismas dimensiones para llevar a cabo las mult.
     for i in range(self.shape[0]):
-      for j in range(otra.shape[1]):
-        for k in range(self.shape[1]):
-          resultado[i][j] += self.valores[i][k]*otra.valores[k][j]
-      return Matriz(resultado)
-    resultado = [[0]*otra.shape[0] for _ in range(self.shape[1])]
-    for i in range(self.shape[0]):
-      for j in range(self.shape[1]):
-        resultado[i][j] = self.valores[i][j]*otra
+        for j in range(otra.shape[1]):
+            for k in range(self.shape[1]):
+                resultado[i][j] += self.valores[i][k]*otra.valores[k][j]
+    return Matriz(resultado)
 
   def __rmul__(self, otra):
-    if self.shape[0] != self.shape[1]:
-      print("Las matrices no pueden multiplicarse")
-    resultado = [[0]*otra.shape[0] for _ in range(self.shape[1])] #Matriz llena de 0 con las mismas dimensiones para llevar a cabo las mult.
+    if self.shape[0] != otra.shape[1]:
+        print("Las matrices no pueden multiplicarse")
+        return None
+    resultado = [[0]*otra.shape[1] for _ in range(self.shape[0])] #Matriz llena de 0 con las mismas dimensiones para llevar a cabo las mult.
     for i in range(self.shape[0]):
-      for j in range(otra.shape[1]):
-        for k in range(self.shape[1]):
-          resultado[i][j] += self.valores[i][k]*otra.valores[k][j]
-      return Matriz(resultado)
-    resultado = [[0]*otra.shape[0] for _ in range(self.shape[1])]
-    for i in range(self.shape[0]):
-      for j in range(self.shape[1]):
-        resultado[i][j] = self.valores[i][j]*otra
-    
+        for j in range(otra.shape[1]):
+            for k in range(self.shape[1]):
+                resultado[i][j] += self.valores[i][k]*otra.valores[k][j]
+    return Matriz(resultado)
+  
+  def intercambio(self, fila1, fila2):
+     self.valores[fila1], self.valores[fila2] = self.valores[fila2], self.valores[fila1]
+
+#Primero aplicar Gauss, luego Jordan:
+  def gauss(matriz):
+    for i in range(len(matriz)):
+      pivote = matriz[i][i]
+      if pivote == 0:
+        continue
+      for j in range(i+1) 
+     
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 <h1 align="center">Parcial 1</h1>
 
-**Programa de Física, Universidad del Quindío, Marzo, 2023**
+**Programa de Física, Universidad del Quindío, Marzo, 2023.**
 
-**Estudiante:** Jhon Sebastian García Barrera
+**Estudiante:** Jhon Sebastian García Barrera.
 
 1. Construya un programa que tenga una clase matriz (puede hacerlo con base en lo realizado en clase) que tenga un método implementado para solucionar un sistema de ecuaciones por el método Gauss-Jordan. La dimensión de la matriz es arbitraria y el programa debe decir si tiene solución única, infinitas soluciones o única solución. La clase debe tener un atributo llamado is valid que es una lista cuyo primer elemento es un Booleando indicando si todos los elementos de la matriz son números o hay algún dato inválido. Si el booleano es True, el segundo elemento de la lista sería una liusta vacía, si es False, el segundo elemento de la lista será una lista con los Indices de los elementos de la matriz que están incorrectos. Ayuda: Si desea hagalo con un N máximo de 5
 2. Organice el ejemplo desarrollado en clase (el de matrices) para que los vectores puedan ser ingresados como una sola lista y no como lista de listas.Defina el producto entre matrices, vectores y escalares usando __mul__ y __rmul__ y defina un método para calcular la transpuesta de una matriz y otro uno para determinar si es simétrica.
@@ -11,3 +11,4 @@
 
 El primer día se llevó a cabo el código para matrices cuadradas desde 2x2 hasta 5x5. Un código largo (400 líneas aprox.) pero no difícil, el cual fue una pérdida de tiempo ya que no era útil, pues, el método Gauss-Jordan se puede aplicar no necesariamente a matrices cuadradas y además de eso, solo con un n de 2 hasta 5, por lo que se definió las matrices nxm, con suma y resta de una manera arbitraria usando for anidados. Hasta ahora ha sido lo más demorado, el hecho de entender bien la lógica para desarrollar esa definición desde cero.
 Se implementó mul y rmul para llevar a cabo las multiplicaciones entre matrices y escalares, primero fue esto ya que para el método G-J es necesario la multiplicación.
+Por ahora la proyección es primero definir un método para intercambiar filas, luego hacer gauss por aparte y luego hacer el jordan. Lo que se lleva hasta ahora no ha sido probado en ejemplos, voy suponiendo que el código funciona de buena manera así, después nos ocupamos de los errores que vayan saliendo. Hecho el intercambio de filas, al empezar con Gauss fue necesario primero añadir un método para encontrar el pivote, pues es necesario para seguir.
