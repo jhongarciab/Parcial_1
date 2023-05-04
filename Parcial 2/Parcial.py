5be87fa42d5f5a81856d6598b6d5e277a65ce096
Jhon Sebastian Garcia <jhons.garciab@uqvirtual.edu.co>
2023-05-04 07:33:32 -0500

    Combinación de tablas

--- Parcial 2/Parcial.py
+++ Parcial 2/Parcial.py
@@ -23,22 +23,38 @@ class Conexión:
         self.puerto = '5432'
 
     def crear_tabla(self):
+            with psycopg2.connect(host=self.host, port=self.puerto, user=self.usuario, password=self.password, dbname=self.nombre_bd) as conn:
+                with conn.cursor() as cursor:
+                    for i, j in enumerate(self.archivos):
+                        nombre_tabla = self.nombre_tabla[i]
+                        df = pd.read_csv(j)
+                        columnas = [f'"{i.lower()}" VARCHAR(255)' for i in df.columns]
+                        cursor.execute(f"CREATE TABLE {nombre_tabla} ({', '.join(columnas)})")
+
+                        for _, row in df.iterrows():
+                            values = [f"'{i}'" for i in row.values.tolist()]
+                            cursor.execute(f"INSERT INTO {nombre_tabla} ({', '.join([f'{i.lower()}' for i in df.columns])}) VALUES ({', '.join(values)})")
+
+    def combinar_tablas(self):
+        dfs = []
+        for archivo in self.archivos:
+            df = pd.read_csv(archivo)
+            dfs.append(df)
+
+        df_combinado = pd.concat(dfs, axis=0, join='inner')
+        df_combinado = df_combinado.drop_duplicates(subset=df_combinado.columns[:3])
+        return df_combinado
+    
+    def crear_tabla_combinada(self, df_combinado):
         with psycopg2.connect(host=self.host, port=self.puerto, user=self.usuario, password=self.password, dbname=self.nombre_bd) as conn:
             with conn.cursor() as cursor:
-                for i, j in enumerate(self.archivos):
-                    nombre_tabla = self.nombre_tabla[i]
-                    df = pd.read_csv(j)
-                    columnas = [f'"{i.lower()}" VARCHAR(255)' for i in df.columns]
-                    cursor.execute(f"CREATE TABLE {nombre_tabla} ({', '.join(columnas)})")
+                nombre_tabla = self.nombre_tabla
+                columnas = [f'"{i.lower()}" VARCHAR(255)' for i in df_combinado.columns]
+                cursor.execute(f"CREATE TABLE {nombre_tabla} ({', '.join(columnas)})")
 
-                    for _, row in df.iterrows():
-                        values = [f"'{i}'" for i in row.values.tolist()]
-                        cursor.execute(f"INSERT INTO {nombre_tabla} ({', '.join([f'{i.lower()}' for i in df.columns])}) VALUES ({', '.join(values)})")
+                for _, row in df_combinado.iterrows():
+                    values = [f"'{i}'" for i in row.values.tolist()]
+                    cursor.execute(f"INSERT INTO {nombre_tabla} ({', '.join([f'{i.lower()}' for i in df_combinado.columns])}) VALUES ({', '.join(values)})")
 
                 conn.commit()
         print(f"La tabla '{self.nombre_tabla}' ha sido creada y los datos han sido insertados.")
-
-mi_objeto = Conexión(
-    nombre_tabla=["test1", "test2"],
-)
-mi_objeto.crear_tabla()
