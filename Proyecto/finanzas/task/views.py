64df43d977e49b15ab4b81606032a06313e0fec2
Jhon Sebastian Garcia <jhons.garciab@uqvirtual.edu.co>
2023-06-01 16:03:17 -0500

    documentación

--- Proyecto/finanzas/task/views.py
+++ Proyecto/finanzas/task/views.py
@@ -9,9 +9,21 @@ from .models import Task
 from django.contrib.auth.decorators import login_required
 
 def home(request):
+    """
+    Esta función representa la vista de la página de inicio de la aplicación de tareas. 
+    Renderiza la plantilla 'home.html' y la  devuelve como respuesta.
+    """
     return render(request, 'home.html')
 
 def signup(request):
+    """
+    Esta función maneja tanto la solicitud GET como la solicitud POST para el registro de usuarios. 
+    Si la solicitud es GET, renderiza la plantilla 'signup.html' junto con el formulario de registro UserCreationForm. 
+    Si la solicitud es POST, verifica que las contraseñas ingresadas coincidan. Si coinciden, crea un nuevo usuario utilizando 
+    el modelo User de Django y lo redirige a la vista de tareas ('task'). Si hay un error de integridad, significa que el nombre de 
+    usuario ya existe y se renderiza la plantilla 'signup.html' con un mensaje de error. Si las contraseñas no coinciden, se renderiza 
+    la plantilla 'signup.html' con un mensaje de error.
+    """
     if request.method  == 'GET':
         return render(request, 'signup.html', {
             'form': UserCreationForm
@@ -36,6 +48,12 @@ def signup(request):
         })
     
 def signin(request):
+    """
+    Esta función maneja tanto la solicitud GET como la solicitud POST para el inicio de sesión de los usuarios. Si la solicitud es GET, 
+    renderiza la plantilla 'signin.html' junto con el formulario de inicio de sesión AuthenticationForm. Si la solicitud es POST, autentica 
+    al usuario utilizando el nombre de usuario y la contraseña proporcionados. Si la autenticación es exitosa, inicia sesión en la aplicación 
+    y redirige al usuario a la vista de tareas ('task'). Si la autenticación falla, se renderiza la plantilla 'signin.html' con un mensaje de error.
+    """
     if request.method == 'GET':
         return render(request, 'signin.html',{
             'form': AuthenticationForm
@@ -55,6 +73,11 @@ def signin(request):
     
 @login_required    
 def task(request):
+    """
+    Esta función representa la vista de las tareas pendientes del usuario. Recupera las tareas filtradas por el usuario actual y 
+    donde la fecha de finalización (datecompleted) sea nula. Las tareas se ordenan en orden descendente según la fecha de finalización. 
+    Luego, renderiza la plantilla 'task.html' junto con las tareas recuperadas y las devuelve como respuesta.
+    """
     task = Task.objects.filter(user=request.user, datecompleted__isnull=True).order_by('-datecompleted')
     return render(request, 'task.html', {
         'task': task
@@ -62,6 +85,11 @@ def task(request):
 
 @login_required 
 def task_completed(request):
+    """
+    Esta función representa la vista de las tareas completadas del usuario. Recupera las tareas filtradas por el usuario actual y donde 
+    la fecha de finalización (datecompleted) no sea nula. Luego, renderiza la plantilla 'completed_task.html' junto con las tareas recuperadas 
+    y las devuelve como respuesta.
+    """
     task = Task.objects.filter(user=request.user, datecompleted__isnull=False)
     return render(request, 'completed_task.html', {
         'task': task
@@ -69,6 +97,12 @@ def task_completed(request):
 
 @login_required 
 def create_task(request):
+    """
+    Esta función maneja tanto la solicitud GET como la solicitud POST para crear una nueva tarea. Si la solicitud es GET, 
+    renderiza la plantilla 'create_task.html' junto con el formulario TaskForm vacío. Si la solicitud es POST, valida el 
+    formulario enviado y crea una nueva instancia de Task relacionada con el usuario actual. Si la información proporcionada no 
+    es válida, se renderiza la plantilla 'create_task.html' con un mensaje de error.
+    """
     if request.method == 'GET':
         return render(request, 'create_task.html', {
             'form': TaskForm
@@ -88,6 +122,14 @@ def create_task(request):
 
 @login_required 
 def task_detail(request, task_id):
+    """
+    Esta función maneja tanto la solicitud GET como la solicitud POST para ver y actualizar los detalles de una tarea específica. 
+    Si la solicitud es GET, se recupera la tarea correspondiente al task_id proporcionado, verificando que pertenezca al usuario actual. 
+    Luego, se crea una instancia de TaskForm con los detalles de la tarea y se renderiza la plantilla 'task_detail.html' junto con la tarea 
+    y el formulario. Si la solicitud es POST, se intenta guardar los cambios realizados en el formulario. Si los cambios son válidos, 
+    se redirige al usuario a la vista de tareas ('task'). Si hay un error de valor, significa que los datos ingresados no son válidos y 
+    se renderiza la plantilla 'task_detail.html' con un mensaje de error.
+    """
     if request.method == 'GET':
         task = get_object_or_404(Task, pk=task_id, user=request.user)
         form = TaskForm(instance=task)
@@ -110,6 +152,11 @@ def task_detail(request, task_id):
 
 @login_required 
 def complete_task(request, task_id):
+    """
+    Esta función maneja la solicitud POST para marcar una tarea como completada. Se recupera la tarea correspondiente al task_id proporcionado, 
+    verificando que pertenezca al usuario actual. Si la solicitud es POST, se establece la fecha de finalización de la tarea como el tiempo 
+    actual utilizando timezone.now() y se guarda la tarea. Luego, se redirige al usuario a la vista de tareas ('task').
+    """
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
         task.datecompleted = timezone.now()
@@ -118,6 +165,10 @@ def complete_task(request, task_id):
 
 @login_required 
 def delete_task(request, task_id):
+    """
+    Esta función maneja la solicitud POST para eliminar una tarea. Se recupera la tarea correspondiente al task_id proporcionado, 
+    verificando que pertenezca al usuario actual. Si la solicitud es POST, se elimina la tarea y se redirige al usuario a la vista de tareas ('task').
+    """
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
         task.delete()
@@ -125,6 +176,10 @@ def delete_task(request, task_id):
 
 @login_required 
 def signout(request):
+    """
+    Esta función maneja la solicitud POST para cerrar sesión de un usuario. Cierra la sesión actual utilizando 
+    logout(request) y redirige al usuario a la página de inicio ('home').
+    """
     logout(request)
     return redirect('home')
 
